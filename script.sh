d5d2a5902d3263783d0b555edcbf228c781268d4
JH8459 <kk_ong2233@naver.com>
2024-02-01 23:28:18 +0900

    Modify: script.sh

--- script.sh
+++ script.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
+echo "=> Docker Compose Down..."
+docker compose down
+sleep 2s
+echo "=> Docker System Prune..."
+docker system prune -f
+sleep 2s
+echo "=> Docker Network Prune..."
+docker network prune -f
+sleep 2s
 echo "=> Docker Compose Build..."
 docker compose build
 sleep 2s
 echo "=> Docker Compose Up..."
 docker compose up -d
+echo "=> Docker Volume Prune..."
+docker volume prune -f
+sleep 2s
