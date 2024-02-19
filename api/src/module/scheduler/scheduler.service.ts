2f7fa7293b7b083c6a77b675f2c5d3b46c0dabaf
JH8459 <kk_ong2233@naver.com>
2024-02-19 11:33:15 +0900

    Modify: Change Dockerfile Setting

--- api/src/module/scheduler/scheduler.service.ts
+++ api/src/module/scheduler/scheduler.service.ts
@@ -6,7 +6,7 @@ import { EmailService } from '../notification/email/email.service';
 export class SchedulerService {
   constructor(private readonly emailService: EmailService) {}
 
-  @Cron('0 9 * * 1', { timeZone: 'Asia/Seoul' })
+  @Cron('0 9 * * 0', { timeZone: 'Asia/Seoul' })
   async sendLottoEmailToSubscriberListScheduler(): Promise<void> {
     await this.emailService.sendLottoEmailToSubscriberList();
   }
