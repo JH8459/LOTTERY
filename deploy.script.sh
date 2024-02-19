e1abf97d63c33f8e1dbf11dec5d64fe7de4c8088
JH8459 <kk_ong2233@naver.com>
2024-02-19 11:05:18 +0900

    Modify: Change Action Script Ver.8

--- deploy.script.sh
+++ deploy.script.sh
@@ -11,6 +11,10 @@ sleep 2s
 echo "=> Docker Network Prune..."
 docker network prune -f
 sleep 2s
+echo "=> Docker Image RM..."
+docker image rm jh8459/lottery_api_server
+docker image rm jh8459/lottery_crawler_server
+sleep 2s
 echo "=> Docker Compose Pull..."
 docker compose pull
 sleep 2s
