d0b898bf64d06d7be33a3fc91951c56abae07c14
JH8459 <kk_ong2233@naver.com>
2024-05-03 16:11:01 +0900

    Modify: slack 이벤트 핸들러 변경

--- api/src/module/notification/slack/slack.controller.ts
+++ api/src/module/notification/slack/slack.controller.ts
@@ -1,19 +1,17 @@
-import { Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { ResponseDto } from 'src/common/dto/response.dto';
 import { SLACK } from './swagger/slack.swagger';
+import { SlackService } from './slack.service';
 
 @ApiTags('Slack API')
 @Controller('/slack')
 export class SlackController {
+  constructor(private slackService: SlackService) {}
+
   @ApiOperation(SLACK.POST.API_OPERATION)
   @Post('/events')
-  async slack(): Promise<ResponseDto> {
-    const result: ResponseDto = {
-      message: 'HEALTH Check',
-      data: new Date(),
-    };
-
-    return result;
+  async slack(@Body('challenge') challenge: string): Promise<string> {
+    return challenge;
   }
 }
