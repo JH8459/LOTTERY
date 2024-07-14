756ca54ca7f21d87d8a0d5bbbfdc78e4722121c2
JH8459 <kk_ong2233@naver.com>
2024-07-14 18:23:41 +0900

    Modify: swagger.interface.ts 타입 수정

--- api/src/common/interface/swagger.interface.ts
+++ api/src/common/interface/swagger.interface.ts
@@ -1,8 +1,9 @@
-import { ApiBodyOptions, ApiOperationOptions } from '@nestjs/swagger';
+import { ApiBodyOptions, ApiOperationOptions, ApiResponseMetadata } from '@nestjs/swagger';
 
 export interface SwaggerMethod {
   GET?: {
     API_OPERATION: ApiOperationOptions;
+    API_RESPONSE: ApiResponseMetadata;
   };
 
   POST?: {
