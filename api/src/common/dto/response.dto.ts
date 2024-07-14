7710294a6230512de5cbd5787c52def29629a3ed
JH8459 <kk_ong2233@naver.com>
2024-07-14 22:18:41 +0900

    Modify: ResponseDto 수정 (@ApiProperty 삭제)

--- api/src/common/dto/response.dto.ts
+++ api/src/common/dto/response.dto.ts
@@ -1,12 +1,9 @@
-import { ApiProperty } from '@nestjs/swagger';
 import { IsOptional, IsString } from 'class-validator';
 
 export class ResponseDto {
-  @ApiProperty({ type: String, description: '응답 메시지' })
   @IsString()
   message: string;
 
-  @ApiProperty({ type: Object, description: '응답 데이터 객체' })
   @IsOptional()
-  data?: object | string;
+  data?: object;
 }
