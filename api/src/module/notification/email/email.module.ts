65455ae29b873d8b9c0bebf5405305237eba3619
JH8459 <kk_ong2233@naver.com>
2025-04-16 09:27:36 +0900

    Modify: 이메일 발송 실패시 슬랙 웹훅 알림 로직 추가

--- api/src/module/notification/email/email.module.ts
+++ api/src/module/notification/email/email.module.ts
@@ -3,9 +3,10 @@ import { EmailService } from './email.service';
 import { EmailController } from './email.controller';
 import { BullModule } from '@nestjs/bull';
 import { EmailProcessor } from './email.processor';
+import { SlackModule } from '../slack/slack.module';
 
 @Module({
-  imports: [BullModule.registerQueue({ name: 'emailQueue' })],
+  imports: [BullModule.registerQueue({ name: 'emailQueue' }), SlackModule],
   providers: [EmailService, EmailProcessor],
   controllers: [EmailController],
   exports: [EmailService],
