2030c56559866968df28c283236f16794a2f3108
JH8459 <kk_ong2233@naver.com>
2025-04-27 11:43:03 +0900

    Modify: slack.e2e-spec.ts 테스트 코드 추가 (SlackController E2E '/slack/auth' - 2)

--- api/test/e2e/slack.e2e-spec.ts
+++ api/test/e2e/slack.e2e-spec.ts
@@ -75,4 +75,21 @@ describe('SlackController E2E - /slack/auth', () => {
     // Assert (리다이렉트 URL 검증)
     expect(result).toBe(`https://${mockTeamDomain}.slack.com/app_redirect?app=${mockAppId}`);
   });
+
+  it(`❌ authorizeSlackCode - auth.service.ts의 fetchOAuthInfo 메서드 인증 실패 시 'https://slack.com'으로 Redirect URL을 반환하는가?`, async () => {
+    // 1. Slack OAuth Access Token API Mock
+    jest.spyOn(httpService, 'post').mockReturnValueOnce(
+      of({
+        data: {
+          ok: false,
+        },
+      } as AxiosResponse)
+    );
+
+    // Act
+    const result = await slackService.authorizeSlackCode('test-code');
+
+    // Assert (리다이렉트 URL 검증)
+    expect(result).toBe('https://slack.com');
+  });
 });
