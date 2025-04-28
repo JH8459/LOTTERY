5aa4a0e37b11ae95538885b4f3e096f9e32f5103
JH8459 <kk_ong2233@naver.com>
2025-04-28 11:09:34 +0900

    Modify: Github Action 테스트 (typeorm config 로깅)

--- api/src/config/typeorm.config.ts
+++ api/src/config/typeorm.config.ts
@@ -1,20 +1,26 @@
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { TypeOrmModuleAsyncOptions } from '@nestjs/typeorm';
+import { TypeOrmModuleAsyncOptions, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { join } from 'path';
 
 export const TYPE_ORM_CONFIG: TypeOrmModuleAsyncOptions = {
   imports: [ConfigModule],
   inject: [ConfigService],
-  useFactory: (configService: ConfigService) => ({
-    type: 'mariadb',
-    host: configService.get<string>('DB_HOST'),
-    port: configService.get<number>('DB_PORT'),
-    username: configService.get<string>('DB_USER'),
-    password: configService.get<string>('DB_PASSWORD'),
-    database: configService.get<string>('DB_DATABASE'),
-    charset: 'utf8mb4',
-    entities: [join(__dirname, '../entity/**/*.entity{.ts,.js}')],
-    synchronize: configService.get<string>('NODE_ENV') === 'test',
-    logging: false,
-  }),
+  useFactory: (configService: ConfigService) => {
+    const config: TypeOrmModuleOptions = {
+      type: 'mariadb',
+      host: configService.get<string>('DB_HOST'),
+      port: configService.get<number>('DB_PORT'),
+      username: configService.get<string>('DB_USER'),
+      password: configService.get<string>('DB_PASSWORD'),
+      database: configService.get<string>('DB_DATABASE'),
+      charset: 'utf8mb4',
+      entities: [join(__dirname, '../entity/**/*.entity{.ts,.js}')],
+      synchronize: configService.get<string>('NODE_ENV') === 'test',
+      logging: true,
+    };
+
+    console.log('📦 [TYPEORM CONFIG]:', config);
+
+    return config;
+  },
 };
